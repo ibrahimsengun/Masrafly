b7e5fa366118ae415ed7c665754d4aa62ee635ba
ibrahimsengun <ibrahimsengun173@gmail.com>
2024-12-13 19:08:12 +0300

    chore: removed unnecesarry code

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -4,20 +4,8 @@ import { getSourcesAction } from '@/actions/source-actions';
 import { CategoryProvider } from '@/context/category-context';
 import { PreferencesProvider } from '@/context/preferences-context';
 import { SourceProvider } from '@/context/source-context';
-import { createClient } from '@/utils/supabase/server';
-import { redirect } from 'next/navigation';
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
-  const supabase = await createClient();
-
-  const {
-    data: { user }
-  } = await supabase.auth.getUser();
-
-  if (!user) {
-    return redirect('/sign-in');
-  }
-
   const sources = await getSourcesAction();
   const categories = await getCategoriesAction();
   const preferences = await getPreferencesAction();
